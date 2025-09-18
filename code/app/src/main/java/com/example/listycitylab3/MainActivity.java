ccf93c24fa13b7eed020a37f31674869b41be283
sharum-m <mazharud@ualberta.ca>
2025-09-18 17:09:17 -0600

    updated code folder

--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,36 +1,50 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener {
 
-    private ArrayList<String> dataList;
+    private ArrayList<City> dataList;
+    private CityArrayAdapter cityAdapter;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
-        };
+        // Initial cities
+        String[] cities    = { "Edmonton", "Vancouver", "Toronto" };
+        String[] provinces = { "AB", "BC", "ON" };
 
         dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
-        
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
+        // Setup ListView + Adapter
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+        // Setup FloatingActionButton
+        FloatingActionButton addButton = findViewById(R.id.button_add_city);
+        addButton.setOnClickListener(v -> {
+            AddCityFragment fragment = new AddCityFragment();
+            fragment.show(getSupportFragmentManager(), "AddCityFragment");
+        });
+    }
+
+    @Override
+    public void addCity(City city) {
+        // Add new city from fragment input
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
     }
 }
